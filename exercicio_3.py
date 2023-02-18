f3b08f0a1eb5cc53e8e95d389cf9ad6bf4ca62e1
junior <118004363+stratznovich@users.noreply.github.com>
2023-02-18 19:42:51 -0300

    Peça ao usuário para digitar três valores inteiros e imprima a soma deles.

---
+++ exercicio_3.py
@@ -0,0 +1,5 @@
+numero1 = int(input("digite os numerais1"))
+numero2 = int(input("digite os numerais2"))
+numero3 = int(input("digite os numerais3"))
+soma = numero1 + numero2 + numero3
+print(f"resultado: {soma}")
