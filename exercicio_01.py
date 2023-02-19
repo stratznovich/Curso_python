495c0b457681dc6e85cd4f1469fec5b7b6727dd7
junior <118004363+stratznovich@users.noreply.github.com>
2023-02-18 22:45:02 -0300

    Faça um programa. que leia um número inteiro e o imprima

---
+++ exercicio_01.py
@@ -0,0 +1,10 @@
+# 1. Faça um programa que leia um número inteiro e o imprima.
+
+
+
+# 3.faca o usuário para digitar três valores inteiros e imprima a soma deles
+
+for contador in range(1,4):
+    contador = int(input(f'{contador} insira 3 valores: '))
+    conta = contador + contador + +contador
+    print(f'resultado:{conta}')
