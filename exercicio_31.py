e71e7cbd2bd3c74a03f472c5573b714b1a1da4ad
junior <118004363+stratznovich@users.noreply.github.com>
2023-02-23 20:13:16 -0300

    Leia um número inteiro e imprima o seu antecessor e o seu sucessor.

--- exercicio_31.py
+++ exercicio_31.py
@@ -2,7 +2,7 @@ numero = int(input("insira um numero: "))
 
 maior = numero + 1
 menor = numero - 1 
-print(f'resultado: {menor}')
-print(f'resultado: {maior}')
+print(f'resultado: {menor}' )
+print(f'resultado: {maior}' )
 
 
