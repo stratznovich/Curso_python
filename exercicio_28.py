11493022a35d54b24feb1b5873dbde94c55c912b
junior <118004363+stratznovich@users.noreply.github.com>
2023-02-23 20:06:09 -0300

    Faça a leitura de três valores e apresente como resultado a soma dos quadrados dos três valores lidos.

--- exercicio_28.py
+++ exercicio_28.py
@@ -3,6 +3,6 @@ V_1 = int(input('insira um valor:'))
 V_2 = int(input('insira um valor:'))
 V_3 = int(input('insira um valor:'))
 R = V_1 * V_1 + V_2 * V_2 + V_3 * V_3
-print(f'resultado: {R}')
+print(f'resultado:{R}')
 
 
